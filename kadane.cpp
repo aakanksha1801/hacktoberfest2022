bf39c26e95906a7a2dc94d10b32d1e0866c0226f
aakanksha1801 <84894401+aakanksha1801@users.noreply.github.com>
2022-10-22 23:38:58 +0530

    Add files via upload

---
+++ kadane.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+
+    /*int n; //kadane algo
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+
+    }
+
+    /* int maxsum=INT_MIN;
+    int currentSum=0;
+    for (int i = 0; i < n; i++)
+    {
+       currentSum+=arr[i];
+       if (currentSum<0)
+       {
+           currentSum=0;
+       } 
+       maxsum=max(maxsum,currentSum);
+    }
+    cout<<maxsum<<endl;*/
+int kadane(int arr[],int n){
+    int currentsum=0;
+    int maxsum=INT_MIN;
+    for (int i = 0; i < n; i++)
+    { 
+        currentsum+=arr[i];
+        if(currentsum<0){
+            currentsum=0;
+        }
+        maxsum=max(maxsum,currentsum);
+    } 
+    return maxsum;
+}
+
+
+
+    int main(){
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i = 0; i <n; i++)
+        {
+            cin>>arr[i];
+        }
+    int wrapsum;
+    int nonwrapsum;
+     nonwrapsum= kadane(arr,n);
+     int totalsum=0;
+     for (int i = 0; i < n; i++)
+     {    totalsum+=arr[i];
+         arr[i]=-arr[i];
+     }
+    wrapsum= totalsum + kadane(arr,n);
+    cout <<max(wrapsum,nonwrapsum) <<endl;
+        return 0;
+    }
